796e3f0453b2244e79d3e7c0d4f82ac75cc950a1
Rintim <rintim@foxmail.com>
2024-05-16 01:16:35 +0800

    feat: zip.

--- lib/net/fetch/avgasd/mod.tsx
+++ lib/net/fetch/avgasd/mod.tsx
@@ -1,14 +1,14 @@
-import JSZip from "jszip";
 import { h, render } from "preact";
 
 import type { FileData, FileList } from "./type";
 import { App as DownloadApp } from "./download";
+import { App as ZipApp } from "./zip";
 
 export async function downloadFiles(
 	files: Record<string, FileData>,
 	name: string = "game",
 	body: HTMLElement = document.body,
-) {
+): Promise<void> {
 	let fileList = Object.entries(files);
 
 	let bodyChildElements = Array.from(body.childNodes).filter(
@@ -17,7 +17,19 @@ export async function downloadFiles(
 	let originHiddenStatus = init(bodyChildElements);
 
 	let fileContents = await download(fileList, body);
-	console.log(fileContents);
+
+	let zippedFile = await zip(fileContents, body);
+
+	let url = URL.createObjectURL(zippedFile);
+	let a = document.createElement("a");
+	a.href = url;
+	a.download = `${name}.zip`;
+	a.addEventListener("click", () => {
+		setTimeout(() => {
+			URL.revokeObjectURL(url);
+		}, 100);
+	});
+	a.click();
 }
 
 function init(children: HTMLElement[]) {
@@ -38,6 +50,22 @@ async function download(map: FileList, body: HTMLElement = document.body): Promi
 
 	let result = await new Promise<Map<string, Blob>>(
 		resolve => void render(<DownloadApp files={map} onFinished={resolve} />, element),
+	);
+
+	element.remove();
+	return result;
+}
+
+async function zip(map: Map<string, Blob>, body: HTMLElement = document.body): Promise<Blob> {
+	let element = document.createElement("main");
+
+	element.style.width = "100%";
+	element.style.height = "100%";
+	element.style.backgroundColor = "white";
+	body.appendChild(element);
+
+	let result = await new Promise<Blob>(
+		resolve => void render(<ZipApp files={map} onFinished={resolve} />, element),
 	);
 
 	element.remove();
---
+++ lib/net/fetch/avgasd/zip.tsx
@@ -0,0 +1,52 @@
+import JSZip from "jszip";
+import { h, Fragment, JSX, Component, ComponentChild, RenderableProps } from "preact";
+
+export class App extends Component<ZipProps, ZipState> {
+	componentDidMount() {
+		let files = this.props.files;
+
+		let zip = new JSZip();
+		let records = new Map();
+		records.set("", zip);
+		for (let [name, data] of files) {
+			let folders = name.split("/");
+			name = folders[folders.length - 1];
+			folders = folders.slice(0, folders.length - 1);
+
+			let now = zip;
+			for (let i = 1; i <= folders.length; ++i) {
+				let current = folders.slice(0, i);
+				if (records.has(current)) now = records.get(current);
+				else {
+					now = now.folder(folders[i - 1]);
+					records.set(current, now);
+				}
+			}
+
+			now.file(name, data);
+		}
+
+		zip.generateAsync({ type: "blob" }, ({ percent, currentFile }) => {
+			this.setState({ percent, currentFile });
+		}).then(this.props.onFinished);
+	}
+
+	render(_props?: RenderableProps<ZipProps, any>, state?: Readonly<ZipState>): ComponentChild {
+		return (
+			<>
+				<p>Percent: {state.percent.toFixed(2)}%</p>
+				<p>Current File: {state.currentFile}</p>
+			</>
+		);
+	}
+}
+
+export interface ZipProps {
+	files: Map<string, Blob>;
+	onFinished: (zipFile: Blob) => void;
+}
+
+export interface ZipState {
+	percent: number;
+	currentFile: string;
+}
